347818469db5faae18d8e49ed5518dc554a45f57
Ahmed Ashraf <agxgv7573@gmail.com>
2024-03-11 11:39:52 +0200

    Making the regex and taking the variables of the numbers

--- T3/calc.cpp
+++ T3/calc.cpp
@@ -1,5 +1,7 @@
 #include<iostream>
 #include <regex>
+#include <string>
+#include <ctype.h>
 using namespace std;
 
 int indS(string t){
@@ -22,22 +24,53 @@ int indS(string t){
 // }
 
 int main(){
-	regex lineRegex("-?[0-9]*[+-/*]?-?[0-9]* [+-/*] -?[0-9]*[+-/*]?-?[0-9]*");
-	regex rational("-?[0-9]*[+-/*]?-?[0-9]*");
-	regex midOperation(" [*/+-] ");
+	regex lineRegex("(-?[0-9]*[+-/*]?-?[0-9]*) ([+-/*]) (-?[0-9]*[+-/*]?-?[0-9]*)");
+	regex rational("(-?[0-9]*)([/]?)(-?[0-9]*)");
+	// regex midOperation(" [*/+-] ");
 	regex operation("[-+/*]");
-	regex number("-?[1-9][0-9]*");
+	regex number("(-?[1-9][0-9]*)");
 	smatch matchingRationals;
 	string text;
 	text = "-123/42 * 12/3";
 
 	int num1,num2;
 	char mOperation;
+	double finalNum;
 
+	if (regex_search(text, matchingRationals, lineRegex)){
+		// Getting the two rationals from the line
+		string minForm[2] = {matchingRationals[1],matchingRationals[3]};
+		// The operation between the two rationals
+		string mainOperation = matchingRationals[2];
+		// Preparing the to rational values'variables
+		double nums[2];
+		for (int i = 0; i < 2; i++)
+		{
+			/* code */
+			smatch matchingNumbers;
+			if(regex_search(minForm[i],matchingNumbers,rational)){
+				nums[i] = (double)stoi(matchingNumbers[1]) / stoi(matchingNumbers[3]);
+			} else if (regex_match(minForm[i],number)){
+				nums[i] = stoi(minForm[i]);
+			}
+			// cout << nums[i] << " \n";
+		}
+		if (mainOperation == "/"){
+			finalNum = nums[0] / nums[1];
+		}
+		else if (mainOperation == "*"){
+			finalNum = nums[0] * nums[1];
+		}
+		else if (mainOperation == "-"){
+			finalNum = nums[0] - nums[1];
+		}
+		else if (mainOperation == "+"){
+			finalNum = nums[0] + nums[1];
+		}
+		cout << finalNum;
 
-	regex_search(text.cbegin(),text.cend(), matchingRationals, rational);
-	for(string i: matchingRationals){
-		cout << i << '\n';
+	} else {
+		cout << "Please insert a valid calculation";
 	}
 
 }
