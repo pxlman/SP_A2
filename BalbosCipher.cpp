f3bc67ea09fe28a697a23662dad43aac40f121e4
Islam Alshaer <islamalshaer.b@gmail.com>
2024-03-19 01:20:21 +0200

    fixed the functions problem now there is no main

--- BalbosCipher.cpp
+++ BalbosCipher.cpp
@@ -15,7 +15,7 @@ Complexity O()
 
 //a function to check if the char is valid (only a digit)
 bool isvalidchar(string s){
-    if (s.size() > 1 || s == "" || not isdigit(s[0]))
+    if (s.size() != 1 || not isdigit(s[0]))
         return false;
     return true;
 }
@@ -56,61 +56,36 @@ string get_chars_number(char ch, vector<vector<char>> matrix){
             if (ch == matrix[i][j]){
                 result += matrix[i][0];
                 result += matrix[0][j];
-                break;
+                return result;
             }
         }
     }
 
-    return result;
 }
 
 
-void take_input_polybius(string message, bool cipher){
+//this function takes 1 if you want to cipher and takes 0 if you want to decipher (as the second parametar)
+string take_input_polybius(bool cipher){
+
+    cout << "enter the message you want to encrypt:  ";
+    string message;
+    getline(cin, message);
 
     //in case we want to cipher we check if every char is valid (alphabetic)
     if (cipher){
         while (not isvalidmessage(message)){
-            cout << "invalid input, please grow up  ";
+            cout << "invalid input, please grow up:  ";
             getline(cin, message);
         }
     }
     //in case of decipher we check if all are numbers
     else{
         while (not isvalid_encrypted(message)){
-            cout << "invalid input, please grow up  ";
+            cout << "invalid input, please grow up:  ";
             getline(cin, message);
         }
     }
-
-}
-
-
-string polybius_cipher(string message, vector<vector<char>> matrix){
-    //looping on every char to encrypt it
-    string encrypted;
-    for (char ch : message){
-        if (ch == ' '){
-            encrypted += ' ';
-            continue;
-        }
-        //all are upper case according to problem's definition
-        ch = toupper(ch);
-        encrypted += get_chars_number(ch, matrix);
-    }
-    return encrypted;
-}
-
-//a function to seperate every word and put them in a vector
-vector<string> get_words(string sentence){
-
-    vector<string> words;
-    istringstream iss(sentence);
-    string word;
-    while (iss >> word) {
-        words.push_back(word);
-    }
-
-    return words;
+    return message;
 
 }
 
@@ -135,7 +110,80 @@ int search_for_index(char x, vector<vector<char>> matrix, char axis){
 
 }
 
-string polybius_decipher(string encrypted, string key, vector<vector<char>> matrix){
+
+//a function to seperate every word and put them in a vector
+vector<string> get_words(string sentence){
+
+    vector<string> words;
+    istringstream iss(sentence);
+    string word;
+    while (iss >> word) {
+        words.push_back(word);
+    }
+
+    return words;
+
+}
+
+
+void polybius_cipher(){
+    vector<vector<char>> matrix = {{'0', '1', '2', '3', '4', '5'}, {'1', 'A', 'B', 'C', 'D', 'E'}, {'2','F', 'G', 'H', 'I', 'K'}, {'3', 'L', 'M', 'N', 'O', 'P'}, {'4', 'Q', 'R', 'S', 'T', 'U'}, {'5','V', 'W', 'X', 'Y', 'Z'}};
+
+    string message = take_input_polybius(true);
+
+    //getting the key
+    cout << "enter the key (write a number and press enter then the next number)  : ";
+    for (int i = 1; i < 6; ++i) {
+        string x;
+        cin >> x;
+        while(not isvalidchar(x)){
+            cout << "what the hell is wrong with you? just enter a goddamn number\n is it too hard for such an ignorant person to understand that you should enter a number?   ";
+            cin >> x;
+        }
+        //setting first row to be the key
+        matrix[0][i] = x[0];
+        //setting the first column
+        matrix[i][0] = x[0];
+    }
+
+    //looping on every char to encrypt it
+    string encrypted;
+    for (char ch : message){
+        if (ch == ' '){
+            encrypted += ' ';
+            continue;
+        }
+        //all are upper case according to problem's definition
+        ch = toupper(ch);
+        encrypted += get_chars_number(ch, matrix);
+    }
+    cout << encrypted;
+}
+
+
+
+
+void polybius_decipher(){
+
+    vector<vector<char>> matrix = {{'0', '1', '2', '3', '4', '5'}, {'1', 'A', 'B', 'C', 'D', 'E'}, {'2','F', 'G', 'H', 'I', 'K'}, {'3', 'L', 'M', 'N', 'O', 'P'}, {'4', 'Q', 'R', 'S', 'T', 'U'}, {'5','V', 'W', 'X', 'Y', 'Z'}};
+    string encrypted = take_input_polybius(false);
+
+    //getting the key
+    string key;
+    cout << "enter the key (write a number and press enter then the next number)  : ";
+    for (int i = 1; i < 6; ++i) {
+        string x;
+        cin >> x;
+        while(not isvalidchar(x)){
+            cout << "what the hell is wrong with you? just enter a goddamn number\n is it too hard for such an ignorant person to understand that you should enter a number?   ";
+            cin >> x;
+        }
+        key += x;
+        //setting first row to be the key
+        matrix[0][i] = x[0];
+        //setting the first column
+        matrix[i][0] = x[0];
+    }
 
     //initializing variables
     string decrypted;
@@ -156,47 +204,15 @@ string polybius_decipher(string encrypted, string key, vector<vector<char>> matr
         decrypted += ' ';
     }
 
-    return decrypted;
+    cout << decrypted;
 
 }
 
 
 int main() {
-
-
-    string message;
-    getline(cin, message);
-
-    //this function takes 1 if you want to cipher and takes 0 if you want to decipher (as the second parametar)
-    take_input_polybius(message, 0);
-
-    //declaring our matrix with initial values 1 2 3 4 5
-    vector<vector<char>> matrix(6);
-    matrix = {{'0', '1', '2', '3', '4', '5'}, {'1', 'A', 'B', 'C', 'D', 'E'}, {'2','F', 'G', 'H', 'I', 'K'}, {'3', 'L', 'M', 'N', 'O', 'P'}, {'4', 'Q', 'R', 'S', 'T', 'U'}, {'5','V', 'W', 'X', 'Y', 'Z'}};
-    string first_column;
-    string first_row;
-    cout << "enter the key (write a number and press enter then the next number)  : ";
-
-
-    //getting the key
-    string key;
-    for (int i = 1; i < 6; ++i) {
-        string x;
-        cin >> x;
-        while(not isvalidchar(x)){
-            cout << "what the hell is wrong with you? just enter a goddamn number\n is it too hard for such an ignorant person to understand that you should enter a number?   ";
-            cin >> x;
-        }
-        key += x;
-        //setting first row to be the key
-        matrix[0][i] = x[0];
-        //setting the first column
-        matrix[i][0] = x[0];
-    }
-
-
-    cout << polybius_decipher(message, key, matrix);
+    polybius_cipher();
 
 }
 
 //24 31345115 13 35314543 35314543
+//I love       c   plus    plus
